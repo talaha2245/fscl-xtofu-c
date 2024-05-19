91a2c28b9e110b7bb4077edca0ad63c85af49ab7
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-19 14:32:15 -0600

    Update generate-runner.py

--- test/generate-runner.py
+++ test/generate-runner.py
@@ -5,13 +5,13 @@ class TestRunnerGenerator:
     def __init__(self):
         self.directory = os.getcwd()
 
-    def find_test_groups(self):
+    def find_test_groups(self, extension):
         test_groups = set()
         pattern = r'XTEST_DEFINE_POOL\((\w+)\)'
 
         for root, _, files in os.walk(self.directory):
             for file in files:
-                if file.startswith('xtest_') and file.endswith('.c'):
+                if file.startswith('xtest_') and file.endswith('.' + extension):
                     with open(os.path.join(root, file), 'r') as f:
                         content = f.read()
                         matches = re.findall(pattern, content)
@@ -19,43 +19,73 @@ class TestRunnerGenerator:
 
         return list(test_groups)
 
-    def generate_test_runner(self, test_groups):
-        header = """/*
-    ==========================================================================
-    Author: Michael Gene Brockus (Dreamer)
-    Email: michaelbrockus@gmail.com
-    Organization: Fossil Logic
-    Description: 
-        This file is part of the Fossil Logic project, where innovation meets
-        excellence in software development. Michael Gene Brockus, also known as
-        "Dreamer," is a dedicated contributor to this project. For any inquiries,
-        feel free to contact Michael at michaelbrockus@gmail.com.
-    ==========================================================================
-    */
-    // Generated Fossil Logic Test
+    def generate_test_runner(self, test_groups, extension):
+        header = """
+// Generated Fossil Logic Test Runner
+"""
 
-    #include <fossil/xtest.h>
+        if extension == 'c':
+            header += """
+#include <fossil/xtest.h>
 
-    //
-    // XUNIT-GROUP: list of test groups for the runner
-    //"""
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test List
+// * * * * * * * * * * * * * * * * * * * * * * * *"""
+        elif extension == 'cpp':
+            header += """
+#include <fossil/xtest.h>
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test List
+// * * * * * * * * * * * * * * * * * * * * * * * *"""
+        elif extension == 'm':
+            header += """
+#import <fossil/xtest.h>
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test List
+// * * * * * * * * * * * * * * * * * * * * * * * *"""
+        elif extension == 'mm':
+            header += """
+#import <fossil/xtest.h>
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test List
+// * * * * * * * * * * * * * * * * * * * * * * * *"""
 
         extern_pools = '\n'.join([f"XTEST_EXTERN_POOL({group});" for group in test_groups])
 
         runner = """
-    //
-    // XUNIT-TEST RUNNER:
-    //
-    int main(int argc, xstring *argv) {
-        XTEST_CREATE(argc, argv);\n"""
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Runner
+// * * * * * * * * * * * * * * * * * * * * * * * *
+"""
+
+        if extension == 'c':
+            runner += """
+int main(int argc, char **argv) {
+    XTEST_CREATE(argc, argv);\n"""
+        elif extension == 'cpp':
+            runner += """
+int main(int argc, char **argv) {
+    XTEST_CREATE(argc, argv);\n"""
+        elif extension == 'm':
+            runner += """
+int main(int argc, char **argv) {\n
+    XTEST_CREATE(argc, argv);\n"""
+        elif extension == 'mm':
+            runner += """
+int main(int argc, char **argv) {\n
+    XTEST_CREATE(argc, argv);\n"""
 
         import_pools = '\n'.join([f"    XTEST_IMPORT_POOL({group});" for group in test_groups])
 
         footer = """
-        return XTEST_ERASE();
-    } // end of func"""
+    return XTEST_ERASE();
+} // end of func
+"""
 
-        with open('xunit_runner.c', 'w') as file:
+        with open(f'xunit_runner.{extension}', 'w') as file:
             file.write(header)
             file.write("\n")
             file.write(extern_pools)
@@ -66,5 +96,7 @@ class TestRunnerGenerator:
 
 # Example usage:
 generator = TestRunnerGenerator()
-test_groups = generator.find_test_groups()
-generator.generate_test_runner(test_groups)
+extensions = ['c', 'cpp', 'm', 'mm']  # Add more extensions as needed
+for ext in extensions:
+    test_groups = generator.find_test_groups(ext)
+    generator.generate_test_runner(test_groups, ext)
